2d9c94894bc89492b4a70758343c2652a163e80d
Luis Arce <luis.arce22@gmail.com>
2018-10-18 13:16:05 -0400

    Start with the l-projects project

--- src/App.js
+++ src/App.js
@@ -3,25 +3,98 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      projects: [
+        {
+          name: "Football-two",
+          dir: "https://github.com/kapit4n/football-two",
+          features: [
+            { text: "Championships", done: false },
+            { text: "Teams", done: false },
+            { text: "Matches", done: false }
+          ]
+        },
+        {
+          name: "ng-vendei",
+          dir: "https://github.com/kapit4n/ng-vendei",
+          features: [
+            { text: "Shopping cart", done: false },
+            { text: "Products List", done: false },
+            { text: "Orders", done: false }
+          ]
+        },
+        {
+          name: "react-2seller",
+          dir: "https://github.com/kapit4n/react-2seller",
+          features: [
+            { text: "Shopping cart", done: false },
+            { text: "Products List", done: false },
+            { text: "Orders", done: false }
+          ]
+        },
+        {
+          name: "l-projects",
+          dir: "https://github.com/kapit4n/l-projects",
+          features: [
+            { text: "Projects view", done: false },
+            { text: "Project planning", done: false },
+            { text: "Project features", done: false }
+          ]
+        },
+        {
+          name: "time-tracker",
+          dir: "https://github.com/kapit4n/l-tracker",
+          features: [
+            { text: "Register start", done: false },
+            { text: "Register end", done: false },
+            { text: "Calculate", done: false }
+          ]
+        },
+        {
+          name: "t-stock",
+          dir: "https://github.com/kapit4n/t-stock",
+          features: [
+            { text: "Shopping cart", done: false },
+            { text: "Products List", done: false },
+            { text: "Orders", done: false }
+          ]
+        }
+      ]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.projects.map(project => (
+          <div className="hex">
+            <div className="top"></div>
+            <div className="middle">
+              <h2>{project.name}</h2>
+              <ul>
+                {project.features.map(feature => (
+                  <li key={feature.id}>
+                    <label>
+                      <span>{feature.text}</span>
+                    </label>
+                  </li>
+                ))}
+                <li><a href={project.dir}>open</a></li>
+              </ul>
+            </div>
+            <div className="bottom"></div>
+
+
+          </div>
+        ))
+
+        }
+
       </div>
-    );
+    )
   }
 }
 
