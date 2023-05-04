c874000940f70f722bd85e1c49bbba18a03f7c3b
Luis Arce <luis.arce22@gmail.com>
2023-05-04 16:25:09 -0400

    Add more info to details page

--- src/projects/Details.js
+++ src/projects/Details.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import ProjectService from '../services/ProjectsService';
 
+import './Details.css'
+
 export default function Details() {
 
   const { id } = useParams()
@@ -29,9 +31,9 @@ export default function Details() {
 
   return (
     <div>
-      <h1>{project.name}</h1>
       <Link to="/">List</Link>
-      <figure style={{ width: '400px', height: '400px' }}>
+      <h1>{project.name}</h1>
+      <figure style={{ width: '400px' }}>
         <img
           className="img-rounded"
           src={project.img}
@@ -39,6 +41,17 @@ export default function Details() {
           style={{ objectFit: 'fill', width: '100%' }}
         />
       </figure>
+      <p className="description">
+        {project.description}
+      </p>
+      <h3>Skills</h3>
+      <ul>
+        {project?.skills?.map(sk => (<li key={sk}>{sk}</li>))}
+      </ul>
+      <h3>Features</h3>
+      <ul>
+        {project?.features?.map(fch => (<li key={fch}>{fch}</li>))}
+      </ul>
     </div>
   );
 }
